f189e0c2c886fdeaf4b4018bc19f0031bb903ed5
drvinceknight <vincent.knight@gmail.com>
2013-06-03 16:28:48 +0100

    All distribution are now drawn and most common words removed

--- analyse.py
+++ analyse.py
@@ -6,9 +6,67 @@ Script to analyse the downloaded data set of books.
 
 """
 
+from __future__ import division
 import pickle
 import sys
 import nltk
+import collections
+import matplotlib.pyplot as plt
+
+
+def RemoveCommon(dict):
+    """
+    A function to remove common English words from a word frequency dictionary
+
+    Args:
+        dict: a dictionary with strings as keys
+    """
+    commonwords = [e.upper() for e in set(nltk.corpus.stopwords.words('english'))]
+    for key in dict.keys():
+        if key.upper() in commonwords:
+            del dict[key]
+    return dict
+
+
+def PlotDict(dict, title, xlabel, width=1):
+    """
+    A function to plot bar plot of distribution of counts in a dictionary
+
+    Args:
+        dict: A dictionary with numeric values
+        title: A title for the plot (which will also be used as the title for the png file that is output
+        xlable: A title for the x label
+        width: width of the bins
+    """
+    plt.figure()
+    x = [len(e) for e in dict.values()]
+    plt.hist(x, bins=[e - width / 2 for e in range(max(x) + 2)], normed=True)
+    plt.xlabel(xlabel)
+    plt.ylabel("Probability")
+    plt.xlim(width / 2, max(x) + width / 2)
+    plt.title(title)
+    plt.savefig(title.replace(" ", "_") + ".png")
+
+
+def PlotFreqDict(dict, title, xlabel, numberofwords=50, width=.1):
+    """
+    A function to plot bar plot of distribution of frequency
+
+    Args:
+        dict: A dictionary with numeric values
+        title: A title for the plot (which will also be sued as the title for the png file that is output
+        xlabel: A title for the x label
+        numberofwords: The number of most popular words
+        width: width of the bins
+    """
+    x = [e - width for e in range(1, 2 * numberofwords + 1, 2)]
+    plt.figure()
+    plt.bar(x, dict.values()[:numberofwords], width=width, align="center")
+    plt.xticks(x, dict.keys()[:numberofwords], rotation=90)
+    plt.xlabel("Word")
+    plt.ylabel("Frequency")
+    plt.title(title + "\n(%s most common words)" % numberofwords)
+    plt.savefig(title.replace(" ", "_") + ".png", bbox_inches='tight')
 
 
 class Book():
@@ -17,7 +75,7 @@ class Book():
     """
     def __init__(self, row):
         self.title = row[0]
-        self.authors = [e.lstrip().rstrip() for e in row[1].replace(",", " and").replace("And", "and").split(" and ")]
+        self.authors = [e.lstrip().rstrip().upper() for e in row[1].replace(",", " and").replace("And", "and").replace("/", " and ").split(" and ")]
         if self.authors == ['']:
             self.authors = ['Unkown']
         self.link = row[2]
@@ -77,52 +135,45 @@ class Table():
                 else:
                     self.authors[author] = [row.title]
 
-    def CreateFreqDistOfTarget(self):
-        """Returns a dictionary of the word frequency in the target"""
+    def CreateFreqDistOfTarget(self, removecommon=False):
+        """Returns a dictionary of the word frequency in the target
+
+        Args:
+            removecommon: If set to True, will remove common words as defined by the nltk.corpus.stopwords.words set.
+        """
         targets = ""
         for row in self.records:
             targets += row.target
-        self.targetfrq = nltk.FreqDist(targets.replace(".", " ").replace("!", " ").replace(",", " ").split())
+        self.targetfrq = collections.OrderedDict(nltk.FreqDist([e.upper() for e in targets.replace(".", " ").replace("!", " ").replace(",", " ").replace(")", "").replace("(", "").replace("'", "").replace("`", "").split()]))  # Creates a sorted dictionary which is useful for plotting.
+        if removecommon:
+            RemoveCommon(self.targetfrq)
 
-    def CreateFreqDistOfOverview(self):
-        """Returns a dictionary of the word frequency in the overiews"""
+    def CreateFreqDistOfOverview(self, removecommon=False):
+        """Returns a dictionary of the word frequency in the overiews
+
+        Args:
+            removecommon: If set to True, will remove common words as defined by the nltk.corpus.stopwords.words set.
+        """
         overviews = ""
         for row in self.records:
             overviews += row.overview
-        self.overviewfrq = nltk.FreqDist(overviews.replace(".", " ").replace("!", " ").replace(",", " ").split())
+        self.overviewfrq = collections.OrderedDict(nltk.FreqDist([e.upper() for e in overviews.replace(".", " ").replace("!", " ").replace(",", " ").replace(")", "").replace("(", "").replace("'", "").split()]))  # Creates a sorted dictionary which is useful for plotting.
+        if removecommon:
+            RemoveCommon(self.overviewfrq)
 
 
 if __name__ == "__main__":
-    picklefile = "book_list.pickle"
+    picklefile = "book_list.pickle"  # Default file to read in.
     if len(sys.argv) > 1:
         if ".pickle" in sys.argv[1]:
             picklefile = sys.argv[1]
-    Books = Table()
-    Books.ReadFile(picklefile)
-    Books.CreateContributorsDict()
-    Books.CreateAuthorsDict()
-    Books.CreateFreqDistOfOverview()
-    Books.CreateFreqDistOfTarget()
-
-    print "%s books have been contributed." % len(Books)
-    print "---"
-    print ""
-    print "Here are the books by contributors:"
-    for contributor in Books.contributors:
-        print contributor + ":"
-        for e in Books.contributors[contributor]:
-            print "\t", e
-    # Include an analysis of number of contributors
-    # Include an analysis of contributes
-    print "---"
-    print ""
-    print "Here are the books by authors:"
-    for author in Books.authors:
-        print author + ":"
-        for e in Books.authors[author]:
-            print "\t", e
-    # Include an analysis of number of authors
-    # Include an analysis of authors
-
-    #print Books.overviewfrq.keys()
-    #print Books.targetfrq.keys()[:20]
+    Books = Table()  # Create table instance
+    Books.ReadFile(picklefile)  # Read in empty instance
+    Books.CreateContributorsDict()  # Create contributors dictionary
+    PlotDict(Books.contributors, "Distribution of number of contributions", "Number of contributions")  # Plot distribution of contributors
+    Books.CreateAuthorsDict()  # Create Authors dictionary
+    PlotDict(Books.authors, "Distribution of number of books by author", "Number of books")  # Plot distribution of authors
+    Books.CreateFreqDistOfOverview(removecommon=True)  # Create distribution of words in overview, removing common words
+    PlotFreqDict(Books.overviewfrq, "Word count in overview", "word")  # Plot distribution
+    Books.CreateFreqDistOfTarget(removecommon=True)  # Create distribution of words in tart, removing common words
+    PlotFreqDict(Books.targetfrq, "Word count in target", "word")  # Plot distribution
